9c1a1cafe1ea89b055704b16580a67ab4e1c882a
ap3334 <chwil2989@naver.com>
2022-08-01 14:30:18 +0900

    잭슨 개체를 json으로 변경

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,7 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -10,5 +14,20 @@ public class AppTest  {
         int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void 테스트_ObjectMapper() throws JsonProcessingException {
+
+        ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+
+        assertThat(jsonStr).isNotBlank();
+
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim());
+
     }
 }
